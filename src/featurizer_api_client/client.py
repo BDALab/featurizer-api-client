7a14b70c96cfebad305458996d0bfd98506a2cd0
Zoltán Galáž <xgalaz00@gmail.com>
2021-11-09 08:50:55 +0100

    Put the default log directory in the project's root level

--- src/featurizer_api_client/client.py
+++ src/featurizer_api_client/client.py
@@ -27,6 +27,7 @@ class FeaturizerApiClient(object):
             "filename": os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
                 "..",
+                "..",
                 "logs",
                 time.strftime("%Y_%m_%d_client.log")
             )
